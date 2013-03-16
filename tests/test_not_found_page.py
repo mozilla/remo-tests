f06d010cfbcb329f5ae2726554736910c9c1ff56
enikocsokasi <csokasi_eniko@yahoo.com>
2013-03-16 22:52:27 +0200

    Test corrected based on code review.

--- tests/test_not_found_page.py
+++ tests/test_not_found_page.py
@@ -1,12 +1,11 @@
-from unittestzero import Assert
-from pages.not_found import NotFound
-
-__author__ = 'Skadool'
 #!/usr/bin/env python
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from unittestzero import Assert
+from pages.not_found import NotFound
 
 
 class TestNotFoundPage:
@@ -14,5 +13,8 @@ class TestNotFoundPage:
     def test_page_not_found(self, mozwebqa):
         not_found = NotFound(mozwebqa)
         not_found.go_to_inexisting_page()
-        Assert.true(not_found.is_404_div_visible)
-        Assert.equal(not_found.get_404_text(), '404 - Not found')
+        Assert.true(not_found.is_404_error_message_visible)
+        Assert.equal(not_found.get_error_message(), '404 - Not found')
+
+
+
