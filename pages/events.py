0055f2ffbd1a81d9c1900e3d47fc324402f14900
Rebecca Billings <rebeccabillings@Rebeccas-MacBook-Pro.local>
2013-03-20 15:02:35 -0700

    Updated to use go_to_events_page, removed unused imports

--- pages/events.py
+++ pages/events.py
@@ -4,19 +4,19 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
 from selenium.webdriver.common.by import By
 
-from pages.page import Page
 from pages.base import Base
 
 
 class Events(Base):
 
+    _page_title = 'Mozilla Reps - Events'
     _events_map_locator = (By.CSS_SELECTOR, '#map')
 
-    def go_to_eventspage(self):
-        self.selenium.get(self.testsetup.base_url + '/events/')
+    def go_to_events_page(self):
+        self.selenium.get(self.base_url + '/events/')
+        self.is_the_current_page
 
     @property
     def is_events_map_visible(self):
