35ce1c0f3c773700bb0a434ea0ba0439f3ac627d
Rebecca Billings <rebeccabillings@Rebeccas-MacBook-Pro.local>
2013-03-28 13:02:48 -0700

    Added make_response and get_response_code

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -9,6 +9,7 @@ import requests
 from unittestzero import Assert
 from urlparse import urlparse
 
+from pages.base import Base
 from pages.home import Home
 from pages.link_crawler import LinkCrawler
 
@@ -53,8 +54,13 @@ class TestHomePage:
 
         home = Home(mozwebqa)
         favicon_url = home.get_favicon_url
-        r = requests.get(favicon_url, verify=False)
+        page_response = requests.get(favicon_url, verify=False)
+        html = BeautifulStoneSoup(page_response.content)
+        bad_links = []
 
-        Assert.equal(
-            r.status_code, 200,
-            u'request to %s responded with %s status code' % (favicon_url, r.status_code))
+        for link in home.get_favicon_url:
+            favicon_url = self.make_absolute(link['href'], home.get_favicon_url)
+            response_code = self.get_response_code(favicon_url, mozwebqa.timeout)
+            if response_code != requests.codes.ok:
+                bad_links.append('%s is not a valid url - status code: %s.' % (url, response_code))
+        Assert.equal(0, len(bad_links), '%s bad urls found: ' % len(bad_links) + ', '.join(bad_links))
