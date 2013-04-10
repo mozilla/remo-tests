85c6e6e4a170d0dfc27cc19fb474059ff2682671
Rebecca Billings <rebeccabillings@Rebeccas-MacBook-Pro.local>
2013-04-09 17:25:31 -0700

    Added page content verification, fixed the pep 8 whitespace issue, fixed property names for accuracy, added more sidebar link checks.

--- pages/about.py
+++ pages/about.py
@@ -12,17 +12,27 @@ from pages.base import Base
 class About(Base):
 
     _page_title = 'Mozilla Reps - About'
-    _about_sidebar_locator = (By.CSS_SELECTOR, '#about-navigation')
-    _about_content_locator = (By.CSS_SELECTOR, '#about-text h3')
+    _about_page_header_locator = (By.CSS_SELECTOR, '#about-text h3')
+    _about_page_text_locator = (By.CSS_SELECTOR, '#about-text > p')
+    _about_sidebar_faq_locator = (By.CSS_SELECTOR, '#about-navigation.three > nav > ul > li > a[href*="faq"]')
+    _about_sidebar_wiki_locator = (By.CSS_SELECTOR, '#about-navigation.three > nav > ul > li > a[href*="wiki"]')
 
     def go_to_about_page(self):
         self.selenium.get(self.base_url + '/about/')
         self.is_the_current_page
 
     @property
-    def is_about_sidebar_visible(self):
-        return self.is_element_visible(*self._about_sidebar_locator)
+    def is_about_page_header_visible(self):
+        return self.find_element(*self._about_page_header_locator).text
 
     @property
-    def is_about_content_visible(self):
-        return self.find_element(*self._about_content_locator).text
+    def is_about_page_text_visible(self):
+        return self.find_element(*self._about_page_text_locator)
+
+    @property
+    def is_about_sidebar_faq_visible(self):
+        return self.is_element_visible(*self._about_sidebar_faq_locator)
+
+    @property
+    def is_about_sidebar_wiki_link_visible(self):
+        return self.is_element_visible(*self._about_sidebar_wiki_locator)
--- tests/test_about_page.py
+++ tests/test_about_page.py
@@ -13,14 +13,15 @@ from pages.about import About
 class TestAboutPage:
 
     @pytest.mark.nondestructive
-    def test_about_sidebar_is_visible(self, mozwebqa):
+    def test_about_sidebar_content_is_visible(self, mozwebqa):
         about_page = About(mozwebqa)
         about_page.go_to_about_page()
-        Assert.true(about_page.is_about_sidebar_visible)
+        Assert.true(about_page.is_about_sidebar_faq_visible)
+        Assert.true(about_page.is_about_sidebar_wiki_link_visible)
 
     @pytest.mark.nondestructive
-    def test_about_page_content(self,mozwebqa):
-        title = u'About Mozilla Reps'
+    def test_about_page_content(self, mozwebqa):
         about_page = About(mozwebqa)
         about_page.go_to_about_page()
-        Assert.equal(title, about_page.is_about_content_visible)
+        Assert.true(about_page.is_about_page_header_visible)
+        Assert.true(about_page.is_about_page_text_visible)
