93e3e4e9f45034eb0d1a3166e045c591e8239682
Andrei Hutusoru <andreihutusoru@P5069.(none)>
2013-04-12 12:58:22 +0300

    update

--- tests/test_people_page.py
+++ tests/test_people_page.py
@@ -14,7 +14,7 @@ class TestPeoplePage:
     @pytest.mark.nondestructive
     def test_home_page_links(self, mozwebqa):
         crawler = LinkCrawler(mozwebqa)
-        urls = crawler.collect_links('/people', **{'class': 'container'})
+        urls = crawler.collect_links('/people', id ='wrapper')
         bad_urls = []
 
         Assert.greater(len(urls), 0, u'something went wrong. no links found.')
