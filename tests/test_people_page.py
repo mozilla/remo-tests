de0b2eb498c413c95cce229a0534146a2aa8fe0a
Rebecca Billings <rebeccabillings@host-5-172.mv.mozilla.com>
2013-04-17 16:48:07 -0700

    Created new branch with cleaned up people page test, removed randrange and profile.py.

--- tests/test_people_page.py
+++ tests/test_people_page.py
@@ -6,24 +6,22 @@
 
 import pytest
 from unittestzero import Assert
-from pages.link_crawler import LinkCrawler
+
+from pages.people import People
 
 
 class TestPeoplePage:
 
     @pytest.mark.nondestructive
-    def test_home_page_links(self, mozwebqa):
-        crawler = LinkCrawler(mozwebqa)
-        urls = crawler.collect_links('/people', id ='wrapper')
-        bad_urls = []
+    def test_people_map_is_visible(self, mozwebqa):
+        people_page = People(mozwebqa)
+        people_page.go_to_people_page()
+        Assert.true(people_page.is_people_map_visible)
 
-        Assert.greater(len(urls), 0, u'something went wrong. no links found.')
-
-        for url in urls:
-            check_result = crawler.verify_status_code_is_ok(url)
-            if check_result is not True:
-                bad_urls.append(check_result)
-
-        Assert.equal(
-            0, len(bad_urls),
-            u'%s bad links found. ' % len(bad_urls) + ', '.join(bad_urls))
+    @pytest.mark.nondestructive
+    def test_profile_grid_is_visible(self, mozwebqa):
+        people_page = People(mozwebqa)
+        people_page.go_to_people_page()
+        Assert.true(people_page.is_profile_grid_visible)
+        Assert.true(people_page.is_profile_name_visible)
+        Assert.true(people_page.is_profile_image_visible)
