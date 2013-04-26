7b9c407446265cbf955ae01f4544358f27508eaa
Matt Brandt <mbrandt@mozilla.com>
2013-04-26 15:49:03 -0700

    Add FAQ page link crawler test

--- tests/test_faq_page.py
+++ tests/test_faq_page.py
@@ -9,6 +9,7 @@ from unittestzero import Assert
 
 from pages.home import Home
 from pages.faq import FAQ
+from pages.link_crawler import LinkCrawler
 
 
 class TestFAQPage:
@@ -19,3 +20,23 @@ class TestFAQPage:
         home.go_to_homepage()
         faq = home.header.click_faq_link()
         Assert.true(faq.is_faq_sidebar_visible)
+
+        @pytest.mark.skip_selenium
+        @pytest.mark.nondestructive
+        def test_that_links_in_the_faq_page_return_200_code(self, mozwebqa):
+            crawler = LinkCrawler(mozwebqa)
+            urls = crawler.collect_links('/faq/', id='main')
+            bad_urls = []
+
+            Assert.greater(
+                len(urls), 0,
+                'The link crawler did not find any urls to crawl')
+
+            for url in urls:
+                check_result = crawler.verify_status_code_is_ok(url)
+                if check_result is not True:
+                    bad_urls.append(check_result)
+
+            Assert.equal(
+                0, len(bad_urls),
+                '%s bad links found. ' % len(bad_urls) + ', '.join(bad_urls))
