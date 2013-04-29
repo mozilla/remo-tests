11dd6f320fa91d03abee90604ee344358c05e9d9
Matt Brandt <mbrandt@mozilla.com>
2013-04-29 14:26:50 -0700

    fix indentation

--- tests/test_faq_page.py
+++ tests/test_faq_page.py
@@ -21,22 +21,22 @@ class TestFAQPage:
         faq = home.header.click_faq_link()
         Assert.true(faq.is_faq_sidebar_visible)
 
-        @pytest.mark.skip_selenium
-        @pytest.mark.nondestructive
-        def test_that_links_in_the_faq_page_return_200_code(self, mozwebqa):
-            crawler = LinkCrawler(mozwebqa)
-            urls = crawler.collect_links('/faq/', id='main')
-            bad_urls = []
+    @pytest.mark.skip_selenium
+    @pytest.mark.nondestructive
+    def test_that_links_in_the_faq_page_return_200_code(self, mozwebqa):
+        crawler = LinkCrawler(mozwebqa)
+        urls = crawler.collect_links('/faq/', id='main')
+        bad_urls = []
 
-            Assert.greater(
-                len(urls), 0,
-                'The link crawler did not find any urls to crawl')
+        Assert.greater(
+            len(urls), 0,
+            'The link crawler did not find any urls to crawl')
 
-            for url in urls:
-                check_result = crawler.verify_status_code_is_ok(url)
-                if check_result is not True:
-                    bad_urls.append(check_result)
+        for url in urls:
+            check_result = crawler.verify_status_code_is_ok(url)
+            if check_result is not True:
+                bad_urls.append(check_result)
 
-            Assert.equal(
-                0, len(bad_urls),
-                '%s bad links found. ' % len(bad_urls) + ', '.join(bad_urls))
+        Assert.equal(
+            0, len(bad_urls),
+            '%s bad links found. ' % len(bad_urls) + ', '.join(bad_urls))
