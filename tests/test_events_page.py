01f26153c028bf58ca487c47b2ba620b667fa911
Rebecca Billings <rebeccabillings@host-5-172.mv.mozilla.com>
2013-05-01 15:51:22 -0700

    Added link crawler to events page

--- tests/test_events_page.py
+++ tests/test_events_page.py
@@ -8,7 +8,7 @@ import pytest
 from unittestzero import Assert
 
 from pages.events import Events
-
+from pages.link_crawler import LinkCrawler
 
 class TestEventsPage:
 
@@ -31,3 +31,23 @@ class TestEventsPage:
         events_page.go_to_events_page()
         events_page.filter_for(query)
         Assert.equal(u'John Giannelos', events_page.event_profile_owner_text)
+
+    @pytest.mark.skip_selenium
+    @pytest.mark.nondestructive
+    def test_that_links_in_the_events_page_return_200_code(self, mozwebqa):
+        crawler = LinkCrawler(mozwebqa)
+        urls = crawler.collect_links('/events/', id='wrapper')
+        bad_urls = []
+
+        Assert.greater(
+            len(urls), 0,
+            'The link crawler did not find any urls to crawl')
+
+        for url in urls:
+            check_result = crawler.verify_status_code_is_ok(url)
+            if check_result is not True:
+                bad_urls.append(check_result)
+
+        Assert.equal(
+            0, len(bad_urls),
+            '%s bad links found. ' % len(bad_urls) + ', '.join(bad_urls))
